032ba07c15e15822d4a1e1e75c2a3636273dea96
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-12 21:25:08 -0400

    game completed

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // text mesh pro
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max; // allows you to set in the inspector
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher() // will be called by buttons
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower() // will be called by buttons
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
